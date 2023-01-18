936c78cb6c6d8312813ef036be3887812d73aea2
gajulasanthosh <112460724+gajulasanthosh@users.noreply.github.com>
2023-01-18 18:30:48 +0530

    Add files via upload

---
+++ Week7test.sql
@@ -0,0 +1,22 @@
+﻿Use Hr
+go
+
+create proc Insert_Position_Details(@PositionCode char(4),
+									@Description varchar(35),
+									@BudgetStrength int,
+									@year smallInt,
+									@CurrentStrength int)
+									as
+									Begin
+									Insert Position(cPositionCode,vDescription,iBudgetedStrength,siYear,iCurrentStrength)
+									values(@PositionCode,@Description,@BudgetStrength,@year,@CurrentStrength)
+									end
+
+									sp_help position
+
+									select * from Position
+									drop proc Insert_Position_Details
+
+									Exec Insert_Position_Details '0098','Example',25,'2024',29
+
+									drop trigger trg_InsertIntoPositionBackup2
